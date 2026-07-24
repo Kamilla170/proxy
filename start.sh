0647e07ecae56135fed65570dff9be783a395934
Kamilla170 <kamilaimanalieva7@gmail.com>
2026-07-24 21:25:24 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Логин/пароль берём из переменных окружения Railway (PROXY_USER, PROXY_PASS)
 # $PORT Railway подставляет автоматически.
-exec gost -L "socks5://${PROXY_USER}:${PROXY_PASS}@:${PORT}"
+# auto:// отдаёт HTTP и SOCKS5 на одном порту с общим логином/паролем
+exec gost -L "auto://${PROXY_USER}:${PROXY_PASS}@:${PORT}"
